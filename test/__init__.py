a7f0adb4e4b91465cbac88fb8fa46ada331f7d24
Mitesh Patel <Mitesh.Patel@finra.org>
2015-06-10 16:32:59 -0400

    Seperated out the tests

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,18 @@
-__author__ = 'PatelM'
+import unittest
+from elasticd.plugins import BasePlugin
+from elasticd.plugins import ResourceLocator
+from elasticd.plugins import Driver
+from elasticd.plugins import Datastore
+from elasticd.plugin_manager import PluginManager
+import os
+import ConfigParser
+
+def get_test_plugin_manager():
+    config_file = os.path.dirname(os.path.realpath(__file__)) + '/../conf/settings.cfg'
+    config_file = os.path.realpath(config_file)
+
+    config = ConfigParser.ConfigParser()
+    config.read(config_file)
+
+    _plugin_manager = PluginManager(config)
+    return _plugin_manager
--- test/test_driver.py
+++ test/test_driver.py
@@ -1,1 +1,12 @@
-__author__ = 'PatelM'
+from test import *
+
+
+class TestDriver(unittest.TestCase):
+
+    def test_driver(self):
+        _plugin_manager = get_test_plugin_manager()
+        _driver = _plugin_manager.get_driver()
+        self.assertTrue(_driver.update(_driver), "Hello Jasdasdasdohn Doe2")
+
+if __name__ == '__main__':
+    unittest.main()
